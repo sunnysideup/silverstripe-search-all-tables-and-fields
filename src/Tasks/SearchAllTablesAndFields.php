3e7a017a3a7ee717cc0cb89689aa47fb6016606c
Nicolaas <github@sunnysideup.co.nz>
2023-07-05 14:58:01 +1200

    MAJOR: cleanup

--- src/Tasks/SearchAllTablesAndFields.php
+++ src/Tasks/SearchAllTablesAndFields.php
@@ -16,14 +16,18 @@ class SearchAllTablesAndFields extends BuildTask
         *
         * @var string
         */
-    protected $title = 'Search all Tables and Fields using /dev/tasks/search-all-tables-and-fields?q=SEARCHTERM, you can add &r=REPLACETERM to replace the search term with the replace term.';
+    protected $title = 'Search all Tables and Fields using /dev/tasks/search-all-tables-and-fields?s=SEARCHTERM';
 
     /**
      * Description
      *
      * @var string
      */
-    protected $description = 'Goes through ALL the tables and fields and ouputs a list of IDs with matches in the tables and IDs for Rows';
+    protected $description = '
+        To search for a class name, add four backslashes - e.g. MyApp\\\\\\\\MyClass
+        You can add &r=REPLACETERM to replace the search term with the replace term.
+        You can add i=1 to make the search case sensitive.
+        ';
 
     /**
      * Enabled
@@ -51,25 +55,48 @@ class SearchAllTablesAndFields extends BuildTask
         if (!Director::is_cli()) {
             exit('Only works in cli');
         }
+        echo $this->description.PHP_EOL;
         $dbConn = DB::get_conn();
         $dbName = $dbConn->getSelectedDatabase();
-        $searchTerm = Convert::raw2sql($request->getVar('q'));
-        $replaceTerm = Convert::raw2sql($request->getVar('r'));
+        $searchTermRaw = (string) $request->getVar('s');
+        $replaceTermRaw = (string) $request->getVar('r');
+        $caseSensitiveRaw = strtolower((string) $request->getVar('c'));
 
+        $searchTerm = Convert::raw2sql($searchTermRaw);
+        $replaceTerm = Convert::raw2sql($replaceTermRaw);
+        if($replaceTerm) {
+            $caseSensitive =
+                $caseSensitiveRaw !== '0' &&
+                ($caseSensitiveRaw) !== 'false' &&
+                ($caseSensitiveRaw) !== 'no';
+        } else {
+            $caseSensitive =
+                $caseSensitiveRaw === '1' ||
+                ($caseSensitiveRaw) === 'true' ||
+                ($caseSensitiveRaw) === 'yes';
+        }
         // Get all tables in the database
         $sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = '{$dbName}'";
         $tables = DB::query($sql);
-
+        echo '-------------------------------'.PHP_EOL;
+        echo '-------------------------------'.PHP_EOL;
+        echo '-------------------------------'.PHP_EOL;
+        echo 'Searching FOR '.$searchTermRaw.' '.PHP_EOL;
+        if($replaceTermRaw) {
+            echo '... and replacing with '.$replaceTermRaw.PHP_EOL;
+        }
+        echo $caseSensitive ? 'Case Sensitive'.PHP_EOL : 'case insensitive'.PHP_EOL;
+        echo '-------------------------------'.PHP_EOL;
+        echo '-------------------------------'.PHP_EOL;
+        echo '-------------------------------'.PHP_EOL;
         // Loop through all tables
         foreach ($tables as $table) {
-            $tableName = $table["table_name"];
+            $tableName = $table["table_name"] ?? $table['TABLE_NAME'];
 
-            echo '-------------------------------'.PHP_EOL;
-            echo $tableName.PHP_EOL;
-            echo '-------------------------------'.PHP_EOL;
+
 
             // Get all columns in the table
-            $sql = "SELECT column_name FROM information_schema.columns WHERE table_name = '{$tableName}'";
+            $sql = "SELECT column_name FROM information_schema.columns WHERE table_name = '{$tableName}' AND table_schema = '{$dbName}'";
             $columns = DB::query($sql);
 
             // Build the query
@@ -77,23 +104,86 @@ class SearchAllTablesAndFields extends BuildTask
 
             // Loop through all columns and add a condition for each column
             foreach ($columns as $column) {
-                echo '- '.$column.PHP_EOL;
-                $columnName = $column["column_name"];
+                $columnName = $column["column_name"] ?? $column['COLUMN_NAME'];
+                $shownCol = false;
                 $conditions[] = "";
-                $sql = "SELECT ID, $columnName FROM {$tableName} WHERE {$columnName} LIKE '%{$searchTerm}%'";
+                if($caseSensitive) {
+                    $sql = "SELECT \"ID\", \"$columnName\" AS C FROM \"{$tableName}\" WHERE BINARY \"{$columnName}\" LIKE BINARY '%{$searchTerm}%' ORDER BY \"ID\" ASC";
+                } else {
+                    $sql = "SELECT \"ID\", \"$columnName\" AS C FROM \"{$tableName}\" WHERE \"{$columnName}\" LIKE '%{$searchTerm}%' ORDER BY \"ID\" ASC";
+
+                }
                 $results = DB::query($sql);
                 foreach($results as $result) {
-                    echo '  - '.implode('|', $result).PHP_EOL;
+                    if($shownCol === false) {
+                        echo '-------------------------------'.PHP_EOL;
+                        echo $tableName.PHP_EOL;
+                        echo '-------------------------------'.PHP_EOL;
+                        echo '- '.$columnName.PHP_EOL;
+                        $shownCol = true;
+                    }
+                    echo '  - '.$result['ID'].' - '.$this->findWordInString($result['C'], $searchTerm).PHP_EOL;
                     if($replaceTerm) {
-                        $sql = "
-                            UPDATE {$tableName}
-                            SET {$columnName} = REPLACE({$columnName}, '{$searchTerm}', '{$replaceTerm}')
+                        DB::alteration_message('Replacing '.$searchTermRaw.' with '.$replaceTermRaw.' in '.$tableName.'.'.$columnName, 'deleted');
+                        if($caseSensitive) {
+                            $sql = "
+                            UPDATE \"{$tableName}\"
+                            SET \"{$columnName}\" = REPLACE(\"{$columnName}\", '{$searchTermRaw}', '{$replaceTermRaw}')
                             WHERE ID = {$result['ID']}";
+                        } else {
+                            $sql = "
+                            UPDATE \"{$tableName}\"
+                            SET \"{$columnName}\" = CONCAT(
+                                LEFT(\"{$columnName}\", LOCATE(LOWER('{$searchTermRaw}'), LOWER(\"{$columnName}\")) - 1),
+                                '{$replaceTermRaw}',
+                                SUBSTRING(\"{$columnName}\", LOCATE(LOWER('{$searchTermRaw}'), LOWER(\"{$columnName}\")) + LENGTH('{$searchTermRaw}'))
+                            )
+                            WHERE LOWER(\"{$columnName}\") LIKE '%".strtolower($searchTermRaw)."%';
+
+                            ";
+                        }
+
                         DB::query($sql);
                     }
                 }
             }
 
         }
+    }
+
+    protected function findWordInString(string $resultString, string $searchTerm, ?bool $caseSentive = false): string
+    {
+        $output = '';
+        $searchTermLength = strlen($searchTerm);
+        $offset = 0;
+        $resultLength = strlen($resultString);
+        if($resultLength < $searchTermLength + 20) {
+            return $resultString;
+        }
+        $method = 'stripos';
+        if($caseSentive) {
+            $method = 'strpos';
+        }
+        while (($pos = $method($resultString, $searchTerm, $offset)) !== false) {
+            $start = max($pos - 10, 0);
+            $length = $searchTermLength + 20; // 10 characters before + word + 10 characters after
+
+            // If start is less than 10, length needs to be reduced
+            if ($pos < 10) {
+                $length -= (10 - $pos);
+            }
+            if($start + $length > $resultLength) {
+                $length = $resultLength - $start;
+            }
+
+            $snippet = substr($resultString, $start, $length);
+            $output .= '...' . $snippet;
+
+
+            // Update offset to start searching from the end of this match
+            $offset = $pos + $searchTermLength;
+        }
+
+        return $output;
     }
 }
